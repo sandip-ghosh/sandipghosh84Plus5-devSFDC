78705c7271df58688e8861d560da00aba55efec1
sandip-ghosh <sandipghosh84@gmail.com>
2020-08-08 02:14:11 +0530

    Update compareDVMFiles.sh

--- sandipghosh84-5-DEV-SFDC-Project/src/pages/scripts/compareDVMFiles.sh
+++ sandipghosh84-5-DEV-SFDC-Project/src/pages/scripts/compareDVMFiles.sh
@@ -20,6 +20,10 @@ for fn in dir2/*.dvm; do
                 if [[ "$filestr1" == "$filestr2" ]]; then
                         echo $fn
                         #diff --context=1 "$file" "dir2/$fn" >> output.txt
+                        if ! cmp $file $fn >/dev/null 2>&1
+                        then
+                                echo files are different
+                        fi
                         diff --context=1 "$file" "$fn" >> output.txt
                 fi
           done
