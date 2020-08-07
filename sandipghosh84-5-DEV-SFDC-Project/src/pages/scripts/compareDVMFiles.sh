e9396be876f74c457c2f7230c8a9e6885b21ca4b
sandip-ghosh <sandipghosh84@gmail.com>
2020-08-08 01:08:40 +0530

    Update compareDVMFiles.sh

--- sandipghosh84-5-DEV-SFDC-Project/src/pages/scripts/compareDVMFiles.sh
+++ sandipghosh84-5-DEV-SFDC-Project/src/pages/scripts/compareDVMFiles.sh
@@ -9,6 +9,7 @@ echo 2-current directory
 pwd
 for file in dir1/*.dvm; do
         #echo $file
-        diff --context=1 "$file" "dir2/${fn##*/}" >> output.txt
+        #diff --context=1 "$file" "dir2/${fn##*/}" >> output.txt
+        diff --context=1 "$file" "dir2/$fn" >> output.txt
         cat output.txt
 done
