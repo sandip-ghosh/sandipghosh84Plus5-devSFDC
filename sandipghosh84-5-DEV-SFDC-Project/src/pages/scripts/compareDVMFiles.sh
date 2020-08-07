0ec7428ff9fe619ab634d4fb9c78b7a0edc7a959
sandip-ghosh <sandipghosh84@gmail.com>
2020-08-08 00:57:31 +0530

    Update compareDVMFiles.sh

--- sandipghosh84-5-DEV-SFDC-Project/src/pages/scripts/compareDVMFiles.sh
+++ sandipghosh84-5-DEV-SFDC-Project/src/pages/scripts/compareDVMFiles.sh
@@ -1,12 +1,14 @@
 #compare files in two directories
+echo 1-current directory 
 pwd
 cd dir2
 fn=$(ls -t | head -n1)
-echo last committed file --> $fn
+echo $fn
 cd ..
+echo 2-current directory 
 pwd
 for file in dir1/*.dvm; do
-        echo $file
+        #echo $file
         diff "$file" "dir2/${fn##*/}" >> output.txt
         cat output.txt
 done
